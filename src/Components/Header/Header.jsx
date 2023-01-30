c92421ea3357c687ba806653034b0c25ad1035c5
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-01-31 01:00:11 +0600

    Footer, Header sticky Done
    Footer, Header sticky Done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,10 +8,23 @@ import NavMobile from "./NavMobile";
 
 const Header = (props) => {
     const [mobileMenu, setMobileMenu] = useState(false);
+    const [sticky, setSticky] = useState(false);
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            setSticky(true);
+        } else {
+            setSticky(false);
+        }
+    });
 
     return (
         <>
-            <header className="py-5 lg:py-0 absolute top-0 left-0 w-full z-10">
+            <header
+                className={`py-5 lg:py-0 top-0 left-0 w-full z-10 ${
+                    sticky ? "fixed bg-body" : "absolute"
+                }`}
+            >
                 <div className="container">
                     <div className="navbar flex justify-between items-center">
                         <div className="logo w-32 sm:w-36">
