7573826832f2ea467bdf88770a214bc15908b0a9
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-02-08 07:11:59 +0600

    Search Autocomplete
    Search Autocomplete

--- src/Components/Banner/SearchFrom/SearchFrom.jsx
+++ src/Components/Banner/SearchFrom/SearchFrom.jsx
@@ -2,11 +2,16 @@ import React, { useRef } from "react";
 import { useState } from "react";
 import Button from "../../UI/Button/Button";
 
+// Import Autocomplete
+import { suggestions } from "../../../Data/suggestions";
+
 // Import Icon
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { MdKeyboardArrowUp } from "react-icons/md";
+import { AiOutlineStar } from "react-icons/ai";
 
 const SearchFrom = (props) => {
+    // Dropdown
     const [isActive, setIsActive] = useState(false);
 
     const dropdownRef = useRef(null);
@@ -19,9 +24,36 @@ const SearchFrom = (props) => {
 
     const [selected, setSelected] = useState("Buy");
     const options = ["Buy", "Rent"];
+
+    // Search Autocomplete
+    const [value, setValue] = useState("");
+    const [showSuggestion, setShowSuggestion] = useState(false);
+    const suggestionOptions = suggestions.filter((option) =>
+        option.location.toLowerCase().includes(value.toLowerCase())
+    );
+
+    const searchRef = useRef(null);
+
+    window.addEventListener("click", (e) => {
+        if (!showSuggestion && !searchRef.current?.contains(e.target)) {
+            setShowSuggestion(false);
+        }
+    });
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+    const suggestionClick = (suggestion) => {
+        setValue(suggestion);
+        setShowSuggestion(false);
+    };
+
     return (
         <form>
-            <div className="bg-white rounded-md md:flex">
+            <div
+                ref={searchRef}
+                className="bg-white rounded-md md:flex relative"
+            >
                 <div
                     ref={dropdownRef}
                     className="p-2 border-r border-solid border-border_color cursor-pointer flex-[0_0_100px] rounded-md relative"
@@ -60,6 +92,9 @@ const SearchFrom = (props) => {
                         className="w-full focus:outline-none h-10 lg:h-12 px-4 text-base text-body font-normal rounded-md border-0 focus:border-0"
                         type="search"
                         placeholder="Search Location..."
+                        value={value}
+                        onChange={handleChange}
+                        onFocus={() => setShowSuggestion(true)}
                     />
                 </div>
                 <div className="flex-shrink-0 flex space-x-4 p-2">
@@ -77,6 +112,31 @@ const SearchFrom = (props) => {
                         </Button>
                     </div>
                 </div>
+                {showSuggestion && (
+                    <div className="absolute top-full z-20 w-full overflow-y-scroll bg-white rounded max-h-[calc(75vh_-_160px)] shadow">
+                        <div className="flex p-3 items-center">
+                            <AiOutlineStar className="text-lg" />
+                            <span className="pl-1">Popular Searches</span>
+                        </div>
+                        <ul className="">
+                            {suggestionOptions.map((option) => (
+                                <li
+                                    key={option.id}
+                                    onClick={() =>
+                                        suggestionClick(option.location)
+                                    }
+                                    className="text-sm font-normal p-3 pr-0 text-body justify-between flex
+                            capitalize hover:bg-[#ececec] hover:font-bold"
+                                >
+                                    {option.location}
+                                    <span className="ml-8 mr-4 whitespace-nowrap">
+                                        {option.city}
+                                    </span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
         </form>
     );
